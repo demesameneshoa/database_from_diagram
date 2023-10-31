a0d23e8ec415ca4d752223cf1efe45ae30692114
demesameneshoa <demesameneshoa@gmail.com>
2023-10-31 23:17:37 +0300

    created patients table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,9 @@
 CREATE DATABASE clinic;
 
 -- create patients table
+CREATE TABLE patients (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    date_of_birth DATE,
+    PRIMARY KEY (id)
+);
