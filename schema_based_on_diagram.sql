50321e98c96e8cd0cecd8b1853a70269fa2a6068
demesameneshoa <demesameneshoa@gmail.com>
2023-10-31 23:29:02 +0300

    Add medical histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -17,3 +17,14 @@ CREATE TABLE treatments (
     name VARCHAR(255),
     PRIMARY KEY (id)
 );
+
+-- create medical_histories table
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at timestamp,
+    patient_id INT,
+    status VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (patient_id) REFERENCES patients (id)
+);
+
